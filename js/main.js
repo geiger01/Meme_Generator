a503ecb20868bcd9b512f9e5cb111502aa88c325
Jonathan Geiger <tantangeiger@gmail.com>
2021-08-11 10:43:33 +0300

    first design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gElCanvas = document.querySelector('.canvas');
+const gCtx = gElCanvas.getContext('2d');
+
+const gCanvasCont = document.querySelector('.meme');
+
+function onInit() {
+  resizeCanvas();
+  addListeners();
+}
+
+function resizeCanvas() {
+  gElCanvas.width = 476;
+  gElCanvas.height = 546;
+}
