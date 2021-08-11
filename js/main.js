c41ca018900b5356a0e6a6158cb038da05c97bb9
Jonathan Geiger <tantangeiger@gmail.com>
2021-08-11 12:22:49 +0300

    text on meme is now displayed on the canvas and it updates the model

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 'use strict';
 
-const gElCanvas = document.querySelector('.canvas');
-const gCtx = gElCanvas.getContext('2d');
-
-const gCanvasCont = document.querySelector('.meme');
+let gElCanvas;
+let gCtx;
 
 function onInit() {
+  gElCanvas = document.querySelector('.canvas');
+  gCtx = gElCanvas.getContext('2d');
   resizeCanvas();
-  addListeners();
+  renderMeme();
 }
 
 function resizeCanvas() {
   gElCanvas.width = 476;
   gElCanvas.height = 546;
+}
+
+function renderMeme() {
+  const meme = getMeme();
+  const memeId = meme.selectedImgId;
+  const memeTxt = meme.lines[meme.selectedLineIdx].txt;
+
+  var img = new Image();
+  img.src = `imgs/${memeId}.jpg`;
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+    renderTxt(memeTxt);
+  };
+}
+
+function renderTxt(txt) {
+  gCtx.font = '50px IMPACT';
+  gCtx.fillStyle = 'white';
+  gCtx.fillText(txt, 20, gElCanvas.height - 480);
+
+  updateMemeTxt(txt);
 }
--- js/meme.service.js
+++ js/meme.service.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var gImgs = [{ id: 1, url: 'imgs/1.jpg', keywords: ['political'] }];
+
+var gMeme = {
+  selectedImgId: 1,
+  selectedLineIdx: 0,
+  lines: [
+    {
+      txt: '',
+      size: 20,
+      align: 'left',
+      color: 'red',
+    },
+  ],
+};
+
+function getMeme() {
+  return gMeme;
+}
+
+function updateMemeTxt(txt) {
+  gMeme.lines[gMeme.selectedLineIdx].txt = txt;
+}
